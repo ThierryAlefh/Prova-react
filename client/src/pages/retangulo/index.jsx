dce0776ef03e81a66ee2ed208e107038e0689c69
othierrydaora <thierryalefh77@gmail.com>
2022-10-18 08:08:59 -0300

    Imagem no retângulo

--- client/src/pages/retangulo/index.jsx
+++ client/src/pages/retangulo/index.jsx
@@ -5,6 +5,7 @@ import './index.sass';
 const Index = () => {
 	const [linhas, setLinhas] = useState(1);
 	const [colunas, setColunas] = useState(1);
+	const [modo, setModo] = useState('asterisco');
 	const [resultado, setResultado] = useState([]);
 
 	function calcular() {
@@ -15,7 +16,7 @@ const Index = () => {
 			alert('A quantidade minima de colunas é 1');
 			return;
 		}
-		setResultado(desenharRetangulo(linhas, colunas));
+		setResultado(desenharRetangulo(linhas, colunas, modo === 'asterisco' ? '*' : '■'));
 	}
 
 	return (
@@ -38,11 +39,29 @@ const Index = () => {
 						min='1'
 					/>
 					<br />
+					<select onChange={e => setModo(e.target.value)}>
+						<option value='asterisco'>Asterisco</option>
+						<option value='quadrado'>Quadrado</option>
+						<option value='imagem'>Imagem</option>
+					</select>
 					<button onClick={calcular}>Verificar</button>
 				</div>
 				<div className='retangulo-total'>
 					{resultado.map(item => {
 						if (item === ' ') return <br />;
+						else if (modo === 'imagem')
+							return (
+								<span>
+									<img
+										style={{
+											width: '50px',
+                                            margin: '5px'
+										}}
+										src='/assets/images/theRock.jfif'
+										alt=''
+									/>
+								</span>
+							);
 						else return <span>{item}</span>;
 					})}
 				</div>
--- client/src/services.js
+++ client/src/services.js
@@ -1,11 +1,12 @@
-export function desenharRetangulo(linhas, colunas) {
+export function desenharRetangulo(linhas, colunas, simbolo) {
 	let rs = [];
 	for (let i = 0; i < linhas; i++) {
 		for (let j = 0; j < colunas; j++) {
-			rs.push('*');
+			rs.push(simbolo);
 		}
 		rs.push(' ');
 	}
 
+	console.log(simbolo);
 	return rs;
 }
